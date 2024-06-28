1579881c410aacc8e327cf5b5205c01fc675ef47
ibrahieMansour <ebrahemansour@gmail.com>
2024-06-28 12:06:04 +0300

    first commit

---
+++ style.js
@@ -0,0 +1,31 @@
+// numbers challenge
+/*
+let a = 1_00,
+  b = 2_00.5,
+  c = 1e2,
+  d = 2.4;
+
+console.log(Math.trunc(Math.min(a, b, c, d))); // minimum value as integer
+
+console.log(a ** Math.trunc(d)); // use a + d to get 10000
+
+console.log(Math.round(d)); // get number 2 from d
+console.log(Math.floor(d));
+console.log(parseInt(d));
+console.log(Math.trunc(d));
+// use b + d
+let x = (Math.trunc(b) / Math.ceil(d)).toFixed(2);
+console.log(x); // 66.67 as string
+console.log(Math.round(x)); // 67 as integer
+*/
+
+// string challenge
+/*
+let str = "Ibrahiem Emad Aldeen";
+
+console.log(str.charAt(2).toUpperCase() + str.slice(3, 8)); // Rahiem (by use [slice, charAt])
+console.log("Z".repeat(10)); // ZZZZZZZZZZ
+console.log(str.split(" ", 1)); // ["Ibrahiem"]
+console.log(str.substring(0, 8), str.substring(str.length - 6)); // Ibrahiem Aldeen (by use substring)
+console.log(str[0].toLowerCase() + str.substring(1, str.length - 1).toUpperCase() + str[str.length - 1]); // iBRAHIEM EMAD ALDEEn
+*/
